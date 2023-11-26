1f700cd781a414803d6c3bc8768ca824ed4dc53a
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2023-11-26 17:05:55 -0300

    início construção dbContext

---
+++ backend/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging());
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
---
+++ backend/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+    }
+}
